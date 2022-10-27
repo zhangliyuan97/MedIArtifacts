8ec4ac69291b0b2c8dcc94983255607de9f4a6a0
Louis <31601242+zhangliyuan97@users.noreply.github.com>
2022-10-27 13:19:00 +0800

    feat: add concat_images function

---
+++ image_utils.py
@@ -0,0 +1,17 @@
+from PIL import Image
+from os.path import join
+
+
+def concat_images(image_files):
+    # image_files: PIL object list
+    COL = 4
+    ROW = 50
+    UNIT_HEIGHT_SIZE = 450
+    UNIT_WIDTH_SIZE = 450
+    SAVE_QUALITY = 50
+
+    target = Image.new('RGB', (UNIT_WIDTH_SIZE * COL, UNIT_HEIGHT_SIZE * ROW))
+    for row in range(ROW):
+        for col in range(COL):
+            target.paste(image_files[COL * row + col], [0 + UNIT_WIDTH_SIZE * col, 0 + UNIT_HEIGHT_SIZE * row])
+    target.save(join(save_dir, "result.png"), quality=SAVE_QUALITY)
