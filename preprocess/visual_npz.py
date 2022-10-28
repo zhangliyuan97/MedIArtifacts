3cec2a65fd70bf2af0aba398fd2233b7682db8b8
Louis <31601242+zhangliyuan97@users.noreply.github.com>
2022-10-28 16:25:28 +0800

    refactor(visual_npz): remove duplicate function

--- preprocess/visual_npz.py
+++ preprocess/visual_npz.py
@@ -1,13 +1,10 @@
 import numpy as np
 from PIL import Image
-
-
-def scale_intensity(arr):
-    arr = arr / (arr.max() - arr.min() + 1e-10)
-    return arr * 255.0
+from ..image_utils import normalize_intensity
 
 
 def visual_arr(img_arr, save_filename):
+    img_arr = normalize_intensity(img_arr)
     img_pil = Image.fromarray(img_arr)
     img_pil = Image.fromarray(img_arr).convert('RGB')
     img_pil.save(save_filename, quality=50)
@@ -15,7 +12,7 @@ def visual_arr(img_arr, save_filename):
 
 def visual_npz(npz_path):
     npz_data = np.load(npz_path)
-    dwi_arr = npz_data['dwi'][10,...]
+    dwi_arr = npz_data['dwi'][10, ...]
     flair_arr = npz_data['flair'][10, ...]
     visual_arr(dwi_arr, "./dwi.png")
     visual_arr(flair_arr, "./flair.png")
