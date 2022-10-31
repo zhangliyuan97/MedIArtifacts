05928fd83bf4930accfeedf762b95b7b38911fe4
Louis <31601242+zhangliyuan97@users.noreply.github.com>
2022-10-31 15:08:18 +0800

    feat(sampler): add sampler for batch balance

---
+++ cls_task/dataloading/sampler.py
@@ -0,0 +1,61 @@
+import math
+import numpy as np
+
+import torch
+from torch.utils.data import Sampler
+import torch.distributed as dist
+
+
+class DistributedWeightedSampler(Sampler):
+    def __init__(self, dataset, num_replicas=None, rank=None, replacement=True, shuffle=True):
+        if num_replicas is None:
+            if not dist.is_available():
+                raise RuntimeError("Requires distributed package to be available")
+        if rank is None:
+            if not dist.is_available():
+                raise RuntimeError("Requires distributed package to be available")
+            rank = dist.get_rank()
+        self.dataset = dataset
+        self.num_replicas = num_replicas
+        self.rank = rank
+        self.epoch = 0
+        self.num_samples = int(math.ceil(len(self.dataset) * 1.0 / self.num_replicas))
+        self.total_size = self.num_samples * self.num_replicas
+        self.replacement = replacement
+        self.shuffle = shuffle
+
+    def calculate_weights(self, targets):
+        class_sample_count = torch.tensor(
+            [(targets == t).sum() for t in np.unique(targets)]
+        )
+        weight = 1. / class_sample_count.double()
+        samples_weight = torch.tensor([weight[t] for t in targets])
+        return samples_weight
+
+    def __iter__(self):
+        g = torch.Generator()
+        g.manual_seed(self.epoch)
+        if self.shuffle:
+            indices = torch.randperm(len(self.dataset), generator=g).tolist()
+        else:
+            indices = list(range(len(self.dataset)))
+
+        indices += indices[:(self.total_size - len(indices))]
+        assert len(indices) == self.total_size
+
+        indices = indices[self.rank:self.total_size:self.num_replicas]
+        assert len(indices) == self.num_samples
+
+        targets = self.dataset.targets
+        targets = torch.tensor(targets)[indices]
+        assert len(targets) == self.num_samples
+        weights = self.calculate_weights(targets)
+        subsample_balanced_indices = torch.multinomial(weights, self.num_samples, self.replacement)
+        dataset_indices = torch.tensor(indices)[subsample_balanced_indices]
+        return iter(dataset_indices.tolist())
+
+    def __len__(self):
+        return self.num_samples
+
+    def set_epoch(self, epoch):
+        self.epoch = epoch
